25a9994783a857ad9d3ed63bd99b24d6bf46dc07
courrierajulie <courrierajulie@hotmail.com>
2023-10-23 17:34:55 -0400

    Ajout javascript (page index.html)

---
+++ js/index.js
@@ -0,0 +1,15 @@
+//Écrire le code Javascript ici
+
+//Ajouter une alerte
+alert("javascript")
+
+//Modifier une image
+function changeImage(){
+    let displayImage = document.getElementById('imagejs')
+    if(displayImage.src.match = ('img/josh-hemsley-unsplash.jpg')){
+    displayImage.src = 'img/josh-hemsley-vn-unsplash.jpg'
+    } else {
+    displayImage.src = 'img/josh-hemsley-unsplash.jpg'
+    }
+} 
+
